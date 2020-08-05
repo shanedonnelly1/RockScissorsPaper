41d924e697584275fecc6fae2a7307bdc9ef30a9
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-08-05 08:37:48 +0100

    Initial Commit

---
+++ RockScissorsPaper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockScissorsPaper
+//
+//  Created by Shane on 05/08/2020.
+//  Copyright © 2020 Shane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
